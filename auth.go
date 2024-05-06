da5ed61a9ee485f30a244f5743e315b1a3932aea
Sergey Mokrov <mokrov_sa@nlmk.com>
2024-05-06 21:58:08 +0300

    добавил методы аутентификации basic и api key

--- auth.go
+++ auth.go
@@ -1,6 +1,7 @@
 package rest_api
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,6 +11,56 @@ import (
 	"strings"
 )
 
+// ApiKeyAuth метод для аутентифкации Basic
+func ApiKeyAuth(Verify func(apiKey string) bool) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		apiKey := c.Request.Header.Get("api-key")
+		if apiKey == "" {
+			ErrorHandler(NewUnauthorizedError(errors.New("authorization api-key not found"), "Authorization api-key not found"), c)
+			return
+		}
+
+		if Verify(apiKey) {
+			ErrorHandler(NewUnauthorizedError(errors.New("authorization api-key is invalid"), "Authorization api-key is invalid"), c)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+// BasicAuth метод для аутентифкации Basic
+func BasicAuth(Verify func(userName string, password string) bool) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			ErrorHandler(NewUnauthorizedError(errors.New("authorization token not found"), "Authorization token not found"), c)
+			return
+		}
+		tokenHeader := strings.Replace(authHeader, "Basic ", "", -1)
+		decodeToken, err := base64.StdEncoding.DecodeString(tokenHeader)
+		if err != nil {
+			ErrorHandler(NewUnauthorizedError(errors.New("authorization token is invalid"), "Authorization token not found"), c)
+			return
+		}
+		claims := strings.Split(string(decodeToken), `:`)
+		if len(claims) > 0 && !Verify(claims[0], claims[1]) {
+			ErrorHandler(NewUnauthorizedError(errors.New("authorization token is invalid"), "Authorization token is invalid"), c)
+			return
+		}
+		marshal, _ := json.Marshal(&struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}{
+			Username: claims[0],
+			Password: claims[1],
+		})
+		c.Set("user", marshal)
+		c.Next()
+	}
+}
+
+// JWTAuth Метод для аутентификации по JWT
 func JWTAuth(GetClaim func() jwt.Claims) func(c *gin.Context) {
 	return func(c *gin.Context) {
 
